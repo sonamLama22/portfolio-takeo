de8276e8db11ab957fc8bc1d7509e797f43e940a
sonamLama22 <lamadsonam19@gmail.com>
2024-03-14 08:31:20 -0400

    remove redundant files and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import Router from "./shared/router";
-import NavBar from "./components/navbar/navbar";
+import Home from "./pages/home/Home";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      {/* <NavBar /> */}
-      <Router />
-    </BrowserRouter>
-  );
+  return <Home />;
 };
 
 export default App;
